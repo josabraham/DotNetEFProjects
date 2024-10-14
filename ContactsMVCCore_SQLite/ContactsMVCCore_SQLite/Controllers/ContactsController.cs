fc14d0ee6d4e26d7dd8f568436eb727f57a4f970
josabraham <josabrahamm@gmail.com>
2024-10-14 15:52:43 +0530

    Contacts Asp.net Core App with Entityframework using SQLite Database

---
+++ ContactsMVCCore_SQLite/ContactsMVCCore_SQLite/Controllers/ContactsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContactsMVCCore.Models;
+
+namespace ContactsMVCCore_SQLite.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private readonly ContactsMVCCoreSQLiteDBContext _context;
+
+        public ContactsController(ContactsMVCCoreSQLiteDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contacts
+        public async Task<IActionResult> Index()
+        {
+            var contactsMVCCoreSQLiteDBContext = _context.Contacts.Include(c => c.ContactGroup);
+            return View(await contactsMVCCoreSQLiteDBContext.ToListAsync());
+        }
+
+        // GET: Contacts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts
+                .Include(c => c.ContactGroup)
+                .FirstOrDefaultAsync(m => m.ContactId == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        // GET: Contacts/Create
+        public IActionResult Create()
+        {
+            ViewData["ContactGroupId"] = new SelectList(_context.ContactGroups, "ContactGroupId", "ContactGroupId");
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ContactId,Name,PhoneNumber,ContactGroupId")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContactGroupId"] = new SelectList(_context.ContactGroups, "ContactGroupId", "ContactGroupId", contact.ContactGroupId);
+            return View(contact);
+        }
+
+        // GET: Contacts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            ViewData["ContactGroupId"] = new SelectList(_context.ContactGroups, "ContactGroupId", "ContactGroupId", contact.ContactGroupId);
+            return View(contact);
+        }
+
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ContactId,Name,PhoneNumber,ContactGroupId")] Contact contact)
+        {
+            if (id != contact.ContactId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contact);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactExists(contact.ContactId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContactGroupId"] = new SelectList(_context.ContactGroups, "ContactGroupId", "ContactGroupId", contact.ContactGroupId);
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts
+                .Include(c => c.ContactGroup)
+                .FirstOrDefaultAsync(m => m.ContactId == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Contacts == null)
+            {
+                return Problem("Entity set 'ContactsMVCCoreSQLiteDBContext.Contacts'  is null.");
+            }
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact != null)
+            {
+                _context.Contacts.Remove(contact);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContactExists(int id)
+        {
+          return (_context.Contacts?.Any(e => e.ContactId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ ContactsMVCCore_SQLite/ContactsMVCCore_SQLite/Models/ContactsMVCCoreSQLiteDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContactsMVCCore.Models
+{
+    public partial class ContactsMVCCoreSQLiteDBContext : DbContext
+    {
+        public ContactsMVCCoreSQLiteDBContext()
+        {
+        }
+        public ContactsMVCCoreSQLiteDBContext(DbContextOptions<ContactsMVCCoreSQLiteDBContext> options)
+            :base(options)
+        {
+            
+        }
+        public virtual DbSet<Contact> Contacts { get; set; }
+        public virtual DbSet<ContactGroup> ContactGroups { get; set; }
+    }
+}
